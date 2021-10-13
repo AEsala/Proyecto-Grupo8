9bc897ba40b8dc180622b3f3b4196886f07aa9c7
jumime <granadosmj@uninorte.edu.co>
2021-10-12 21:02:38 -0500

    Cambios en segunda pagina

--- Sprint#3/app.py
+++ Sprint#3/app.py
@@ -2,8 +2,12 @@ from flask import Flask , render_template
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/docente/buscarActividad')
 def index():
-    return render_template("retroAlimentarActividadBusqueda.html")
+    return render_template("buscarActividad.html")
+
+@app.route('/docente/buscarActividad/retroalimentarActividad')
+def reultado():
+    return render_template("retroActividad.html")        
 
 
