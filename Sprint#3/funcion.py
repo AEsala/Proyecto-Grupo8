23917f24a474965dae32856c9607bb2228698aec
AEsala <aesala@uninorte.edu.co>
2021-10-21 13:54:28 -0500

    Formateo de Archivo App.py

--- Sprint#3/funcion.py
+++ Sprint#3/funcion.py
@@ -5,14 +5,24 @@ from wtforms.validators import DataRequired, length,InputRequired
 
 
 class registro(FlaskForm):
-    nombre =StringField('Nombres :',validators=[DataRequired(message='No dejar vacío, completar')])
-    apellido =StringField('Apellidos :',validators=[DataRequired(message='No dejar vacío, completar')])
-    cedula=StringField('Cedula :',validators=[DataRequired(message='No dejar vacio,completar')])
-    direccion=StringField('Direccion :',validators=[DataRequired(message='No dejar vacío, completar')])
-    email=EmailField('Correo :',validators=[DataRequired(message='No dejar vacío, completar')])
-    celular=StringField('Celular :',validators=[DataRequired(message='No dejar vacío, completar')])
-    enviar=SubmitField('Registrar')
+    nombre = StringField('Nombres:', validators = [DataRequired(message='No dejar vacío, completar')])
+    
+    apellido = StringField('Apellidos:', validators = [DataRequired(message='No dejar vacío, completar')])
+
+    cedula = StringField('Cedula:',  validators = [DataRequired(message='No dejar vacio,completar')])
+
+    direccion = StringField('Direccion:',  validators = [DataRequired(message='No dejar vacío, completar')])
+
+    email = EmailField('Correo:', validators = [DataRequired(message='No dejar vacío, completar')])
+
+    celular = StringField('Celular:', validators = [DataRequired(message='No dejar vacío, completar')])
+
+    enviar = SubmitField('Registrar')
+
+
 class login(Form):
-    usuario=StringField('Usuario :',validators=[DataRequired(message='No dejar vacío, completar')])
-    password=PasswordField('Contraseña :',validators=[DataRequired(message='No dejar vacío, completar')])
-    inicio_sesion=SubmitField('Iniciar Sesión')
+    usuario = StringField('Usuario:', validators = [DataRequired(message='No dejar vacío, completar')])
+
+    password = PasswordField('Contraseña:', validators = [DataRequired(message='No dejar vacío, completar')])
+    
+    inicio_sesion = SubmitField('Iniciar Sesión')
