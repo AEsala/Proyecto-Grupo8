2b26e1b649ab0c7f9956fe4077a67b2d81a37aab
AEsala <aesala@uninorte.edu.co>
2021-10-22 13:06:31 -0500

    Controlador de Login

--- Sprint#3/validarForms.py
+++ Sprint#3/validarForms.py
@@ -1,5 +1,25 @@
 """ Importaciones """
+from flask import jsonify
 from db import get_db
+from werkzeug.security import check_password_hash, generate_password_hash
 
-def iniciarSesion():
-    return "Validar"
+def iniciarSesion(user, pss):
+    if (len(user) >= 5) and (len(pss) >= 8):
+        cursor = get_db().cursor()
+
+        cursor.execute("SELECT * FROM Usuarios WHERE primerNombre = ?", [user])
+        resultado = cursor.fetchone()
+
+        validate = check_password_hash(resultado[7], pss)
+
+        if validate == True:
+            return(jsonify({
+                "datos": resultado,
+                "Inicio": "Correcto"
+            }))
+        else:
+            return(jsonify({
+                "Inicio": "Error"
+            })) 
+    else:
+        return(jsonify("No se pudo iniciar sesión"))
